5a5cdfcf6e012c21f881eeda87bba4b13f43b0cd
EndaltsevaAR <lady.stranger2010@gmail.com>
2024-03-12 22:35:54 +0300

    solution is work correctly, but slow

--- uno-soft/testing/src/main/java/uno/soft/FileProcessor.java
+++ uno-soft/testing/src/main/java/uno/soft/FileProcessor.java
@@ -7,9 +7,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class FileProcessor {
     private String filename;
@@ -31,9 +33,12 @@ public class FileProcessor {
         long startTime = System.currentTimeMillis();
         long freeMemoryBefore = runtime.freeMemory();
 
-        Set<List<String>> lines = readFile(); // здесь будут хранится уникальные подстроки по столбам для каждой
-                                              // группы
-        List<List<Set<String>>> substringGrouping = groupingLines(lines);
+        List<List<String>> lines = readFile();
+        List<List<List<String>>> groupedLines = groupingLines(lines); // лист сверху - это список групп, следущий -
+                                                                      // список строк в группе, последний - список
+                                                                      // подстрок в строке
+
+        printLines(groupedLines);
 
         long endTime = System.currentTimeMillis();
         long freeMemoryAfter = runtime.freeMemory();
@@ -45,13 +50,41 @@ public class FileProcessor {
 
     }
 
-    private List<List<Set<String>>> groupingLines(Set<List<String>> lines) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'groupingLines'");
+    private List<List<List<String>>> groupingLines(List<List<String>> lines) {
+        List<List<List<String>>> groups = new ArrayList<>();
+        while (!lines.isEmpty()) { // мы проходим по всему списку, пока не опустошим его
+            List<List<String>> currentGroup = new ArrayList<>();
+
+            Map<Integer, Set<String>> columnValues = new HashMap(); // обработка первой строки
+            for (int i = 0; i < lines.get(0).size(); i++) {
+                Set<String> values = new HashSet<>();
+                values.add(lines.get(0).get(i));
+                columnValues.put(i, values);
+            }
+
+            boolean isAdded = false;
+
+            while (!isAdded) {
+                isAdded = false;
+                List<List<String>> notUsedLines = new ArrayList<>();
+                for (List<String> line : lines) {
+                    if (isLineAtGroup(line, columnValues)) {
+                        addSubsToSets(line, columnValues);
+                        currentGroup.add(line);
+                        isAdded = true;
+                    } else {
+                        notUsedLines.add(line);
+                    }
+                }
+                lines = notUsedLines;
+            }
+            groups.add(currentGroup);
+        }
+        return groups;
     }
 
-    private Set<List<String>> readFile() throws IOException {
-        Set<List<String>> lines = new TreeSet<>();
+    private List<List<String>> readFile() throws IOException {
+        List<List<String>> lines = new ArrayList<>();
 
         File file = new File(filename);
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
@@ -60,7 +93,7 @@ public class FileProcessor {
                 List<String> splitedLine = Arrays.asList(line.split(";")); // разделенная строк
                 boolean isOk = true;
                 for (String subLine : splitedLine) { // смотрим нет ли неправильных форматов
-                    if (countOccurrences(subLine, '"') != 2) {
+                    if (countOccurrences(subLine, '"') != 2) { // подумать еще варианты падения
                         isOk = false;
                         break;
                     }
@@ -69,8 +102,6 @@ public class FileProcessor {
                     lines.add(splitedLine);
                 }
             }
-            System.out.println(lines.toString());
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -87,4 +118,40 @@ public class FileProcessor {
         return count;
     }
 
+    private boolean isLineAtGroup(List<String> line, Map<Integer, Set<String>> columnValues) {
+        boolean isLineAtGroup = false;
+        Set<Integer> columnIndexes = columnValues.keySet();
+        for (int i = 0; i < line.size(); i++) {
+            if (columnIndexes.contains(i)) {
+                if (columnValues.get(i).contains(line.get(i))) {
+                    return true;
+                }
+            }
+        }
+        return isLineAtGroup;
+    }
+
+    private void addSubsToSets(List<String> line, Map<Integer, Set<String>> columnValues) {
+        Set<Integer> columnIndexes = columnValues.keySet();
+        for (int i = 0; i < line.size(); i++) {
+            Set<String> values = null;
+            if (columnIndexes.contains(i)) {
+                values = columnValues.get(i);
+            } else {
+                values = new HashSet<>();
+            }
+            values.add(line.get(i));
+            columnValues.put(i, values);
+        }
+    }
+
+    private void printLines(List<List<List<String>>> groupedLines) {
+        for (int i = 0; i < groupedLines.size(); i++) {
+            System.out.println("Группа " + (i + 1));
+            for (List<String> line : groupedLines.get(i)) {
+                System.out.println(String.join(";", line));
+            }
+            System.out.println();
+        }
+    }
 }
